e653e6721addd60a549bfdedbb00e62a73f70c9f
ShaunShepherd <shaun.shepherd142@gmail.com>
2023-07-22 00:32:40 +1000

    Created GrowOnLook

---
+++ PlayerExpA1/Assets/Scripts/GrowOnLook.cs
@@ -0,0 +1,11 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GrowOnLook : MonoBehaviour
+{
+    [Header("Look to Grow Variables")]
+    [SerializeField] float growRate;
+
+
+}
